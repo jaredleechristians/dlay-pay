38299bfbf9524667b15d1e329ea6c2deef85e200
root <jaredleechristians@gmail.com>
2022-03-11 23:24:24 +0200

    new file:   install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/jaredleechristians/dlay-pay.git
+sudo apt install python3-django
+sudo apt-get install python3-pip
+sudo pip3 install --upgrade django==3.2.9
+sudo pip install requests
+sudo pip install python-dateutil
