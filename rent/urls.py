3efac72c177dcc58d92916b51f96ec36ef085df4
root <root@test-dlay.us-central1-a.c.sandbox-284420.internal>
2022-04-18 16:46:26 +0000

    update rent urls

--- rent/urls.py
+++ rent/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 	path('',views.index, name="index"),
-	path('member/',views.member,name="member"),
-	path('instalment/',views.instalment,name="instalment"),
-	path('confirm/',views.confirm,name="confirm"),
-	path('checkout/',views.checkout,name="checkout"),
-	path('conclude/',views.conclude,name="conclude"),
+	path('member/',views.member,name="rent_member"),
+	path('instalment/',views.instalment,name="rent_instalment"),
+	path('confirm/',views.confirm,name="rent_confirm"),
+	path('checkout/',views.checkout,name="rent_checkout"),
+	path('conclude/',views.conclude,name="rent_conclude"),
 ]
