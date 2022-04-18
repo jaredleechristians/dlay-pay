1c6c4d2d64a898cc385e6fee1ba5722686286325
root <root@test-dlay.us-central1-a.c.sandbox-284420.internal>
2022-04-18 15:41:05 +0000

    DLAY rent django app ready

--- dlay/settings.py
+++ dlay/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'pay.apps.PayConfig',
+    'rent.apps.RentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- dlay/urls.py
+++ dlay/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('pay.urls')),
+    path('rent/', include('rent.urls')),
     path('admin/', admin.site.urls),
 ]
